f240859cb62e0e4ef919083788a50b938013ed8c
DuyPhuc <duyphuc070104@gmail.com>
2024-07-02 10:11:58 +0700

    Finish layout for Boarding Pass

--- app/src/main/java/com/example/travelapplication/BoardingPassActivity.java
+++ app/src/main/java/com/example/travelapplication/BoardingPassActivity.java
@@ -1,6 +1,7 @@
 package com.example.travelapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.travelapplication.databinding.ActivityBoardingPassBinding;
+
 public class BoardingPassActivity extends AppCompatActivity {
+
+    ActivityBoardingPassBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityBoardingPassBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_boarding_pass);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.boardingBackButton.setOnClickListener(v -> finish());
     }
 }
